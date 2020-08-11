55d49d7656d0c1f789b774cdcb78f73cc99a584d
DESKTOP-9B9BAAF\luisenrique <luisenrique.san.lan@gmail.com>
2020-08-11 02:55:36 -0500

    Instalacion de dependencias NPM, Node, Vue.js y Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,17 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.styles([
-    'resources/plantilla/css/font-awesome.min.css',
-    'resources/plantilla/css/simple-line-icons.min.css',
-    'resources/plantilla/css/style.css',
-    'resources/plantilla/css/estilos_login.css'
-], 'public/css/plantilla.css')
-.scripts([
-    'resources/plantilla/js/jquery.min.js',
-    'resources/plantilla/js/popper.min.js',
-    'resources/plantilla/js/bootstrap.min.js',
-    'resources/plantilla/js/Chart.min.js',
-    'resources/plantilla/js/pace.min.js',
-    'resources/plantilla/js/template.js'
-], 'public/js/plantilla.js');
+mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/sass/app.scss', 'public/css');
